e704751a5234aef2d0f3c78e3ac2799f4fdada31
shaik <shaik@LAPTOP-GMIMKKPP>
2021-11-02 14:58:31 +0530

    find a student

--- SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
+++ SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
@@ -1,10 +1,32 @@
 package dxc.b2.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+	@Autowired
+	StudentService studentService;
+
+	@RequestMapping("/students")
+	public List<Student> getStudents(){
+
+		return studentService.getStudents();
+
+	}
+
+	@RequestMapping("/students/{id}")
+	public Student findStudent(@PathVariable int id){//in url path there's a variable
+
+		return studentService.findStudent(id);
+
+	}
 
 	@GetMapping("/")
 	public String home() {
