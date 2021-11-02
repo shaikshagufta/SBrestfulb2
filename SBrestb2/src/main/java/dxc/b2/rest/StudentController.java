68ccdf111dabe53d46ea23a05dde3a855bd88301
shaik <shaik@LAPTOP-GMIMKKPP>
2021-11-02 10:29:56 +0530

    initial commit

--- SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
+++ SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
@@ -1,5 +1,24 @@
 package dxc.b2.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+
+	@GetMapping("/")
+	public String home() {
+		return ("<h1>welcome home</h1>");
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1>welcome admin</h1>");
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1>welcome user</h1>");
+	}
 
 }
